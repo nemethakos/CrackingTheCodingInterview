391ae59df7df33c6e6a2f0eca08d64c5ecda1758
Ákos Németh <nemeth.akos@gmail.com>
2018-10-13 21:59:18 +0200

    Questions 1.2-1.4

---
+++ src/main/java/Chapter1Question4.java
@@ -0,0 +1,99 @@
+import java.util.HashMap;
+import java.util.Objects;
+
+public class Chapter1Question4 {
+
+	/**
+	 * <p>
+	 * Given a string, write a function to check if it is a permutation of a
+	 * palindrome. A palindrome is a word or phrase that is the same forwards and
+	 * backwards. A permutation is a rearrangement of letters. The palindrome does
+	 * not need to be limited to just dictionary words.
+	 * <p>
+	 * <em>Assumptions:</em>
+	 * <ul>
+	 * <li>ASCII characters are used (a-z)
+	 * <li>White space does not count when checking the palindrome
+	 * </ul>
+	 * <p>
+	 * EXAMPLE Input: Tact Coa
+	 * <p>
+	 * Output: True (permutations: "taco cat", "atco eta", etc.)
+	 *
+	 * @param text the text to be checked
+	 * @return true, if the text could be rearranged to be a palindrome
+	 * @param text
+	 * @return
+	 */
+	public static boolean isPalindromePermutationASCII(String text) {
+		Objects.requireNonNull(text);
+		if (text.length() == 0) {
+			return true;
+		}
+
+		int bits = 0;
+		for (var c : text.toLowerCase().toCharArray()) {
+			if (!Character.isWhitespace(c)) {
+				// index will be between 0(a) and 26(z)
+				int index = (c - 'a');
+				// the indexth bit will be set to 1
+				int mask = 1 << index;
+				// flip the indexth bit
+				bits ^= mask;
+			}
+		}
+
+		// true when all characters occures even times (all bits are zero)
+		boolean allCharsOccursEvenTimes = bits == 0;
+		// true when only one bit set to 1
+		boolean onlyOneBitSetTo1 = ((bits - 1) & bits) == 0;
+
+		return allCharsOccursEvenTimes || onlyOneBitSetTo1;
+	}
+
+	/**
+	 * <p>
+	 * Given a string, write a function to check if it is a permutation of a
+	 * palindrome. A palindrome is a word or phrase that is the same forwards and
+	 * backwards. A permutation is a rearrangement of letters. The palindrome does
+	 * not need to be limited to just dictionary words.
+	 * <p>
+	 * <em>Assumptions:</em>
+	 * <ul>
+	 * <li>Unicode characters are used
+	 * <li>White space does not count when checking the palindrome
+	 * </ul>
+	 * <p>
+	 * EXAMPLE Input: Tact Coa
+	 * <p>
+	 * Output: True (permutations: "taco cat", "atco eta", etc.)
+	 *
+	 * @param text the text to be checked
+	 * @return true, if the text could be rearranged to be a palindrome
+	 */
+	public static boolean isPalindromePermutation(String text) {
+		Objects.requireNonNull(text);
+		if (text.length() == 0) {
+			return true;
+		}
+		var map = new HashMap<Character, Integer>();
+		for (var c : text.toLowerCase().toCharArray()) {
+			if (!Character.isWhitespace(c)) {
+				if (map.get(c) == null) {
+					map.put(c, 0);
+				}
+				map.put(c, map.get(c) + 1);
+			}
+		}
+
+		int oddCount = 0;
+		for (var c : map.keySet().toArray(new Character[0])) {
+			if ((map.get(c) & 1) == 1) {
+				oddCount++;
+			}
+		}
+
+		return oddCount <= 1;
+	}
+
+}
---
+++ src/test/java/Chapter1Question3Test.java
@@ -0,0 +1,39 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Chapter1Question3Test {
+
+	@Test
+	void testUrlifyWithoutInsert() {
+		char[] charArray = "A B C          ".toCharArray();
+		Chapter1Question3.urlifyWithoutInsert(charArray, 5);
+		
+		String result = String.copyValueOf(charArray).toString();
+		
+		System.out.println("urlifyWithoutInsert: '" + result + "'");
+		assertEquals("A%20B%20C", result.trim());
+	}
+	
+	@Test
+	void testCountSpaces() {
+		char[] str = "A B C                 ".toCharArray();
+		int spaces = Chapter1Question3.countSpaces(str, 5);
+		assertEquals(2, spaces);
+	}
+	
+	@Test
+	void testUrlify() {
+		char[] charArray = "Mr John Smith    ".toCharArray();
+		Chapter1Question3.urlifyWithInsert(charArray, 13);
+		assertEquals("Mr%20John%20Smith", String.copyValueOf(charArray).toString());
+	}
+
+	@Test
+	void testMove() {
+		char[] charArray = " MOVE  ".toCharArray();
+		Chapter1Question3.move(charArray, 1, 3, 4);
+		assertEquals(" MOMOVE", String.copyValueOf(charArray).toString());
+	}
+
+}
