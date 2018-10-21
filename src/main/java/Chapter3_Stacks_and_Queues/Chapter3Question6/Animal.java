95ab1d29fa0a2d97a85c791cbbecc5bbff41f280
Ákos Németh <nemeth.akos@gmail.com>
2018-10-22 01:31:49 +0200

    Format changed

--- src/main/java/Chapter3_Stacks_and_Queues/Chapter3Question6/Animal.java
+++ src/main/java/Chapter3_Stacks_and_Queues/Chapter3Question6/Animal.java
@@ -1,17 +1,6 @@
 package Chapter3_Stacks_and_Queues.Chapter3Question6;
 
 public class Animal implements Comparable<Animal> {
-	public int getOrder() {
-		return order;
-	}
-
-	public void setOrder(int order) {
-		this.order = order;
-	}
-
-	public String getName() {
-		return name;
-	}
 
 	protected String name;
 	private int order;
@@ -19,5 +8,16 @@ public class Animal implements Comparable<Animal> {
 	@Override
 	public int compareTo(Animal o) {
 		return o.getOrder() - this.getOrder();
+	}
+
+	public String getName() {
+		return name;
+	}
+	public int getOrder() {
+		return order;
+	}
+
+	public void setOrder(int order) {
+		this.order = order;
 	}
 }
--- src/main/java/Chapter3_Stacks_and_Queues/Chapter3Question6/Cat.java
+++ src/main/java/Chapter3_Stacks_and_Queues/Chapter3Question6/Cat.java
@@ -1,6 +1,7 @@
 package Chapter3_Stacks_and_Queues.Chapter3Question6;
 
 public class Cat extends Animal {
+	
 	public Cat(String name) {
 		this.name = name;
 	}
