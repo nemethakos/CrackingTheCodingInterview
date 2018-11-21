2595dbfa69517fa3eb51f78370b8f7b04e77f7e0
Ákos Németh <nemeth.akos@gmail.com>
2018-11-21 22:26:00 +0100

    5.4 - solutions from the book

---
+++ src/main/java/Chapter_05_Bit_Manipulation/NextNumber.java
@@ -0,0 +1,127 @@
+package Chapter_05_Bit_Manipulation;
+
+/**
+ * <b>Next Number:</b> Given a positive integer, print the next smallest and the
+ * next largest number that have the same number of 1 bits in their binary
+ * representation.
+ */
+public class NextNumber {
+	public static int getNextArithmetic(int number) {
+		int temporaryCounter = number;
+		int countZeros = 0;
+		int countOnes = 0;
+
+		// 00011000 -> 00011 -> 3 trailing zeros
+		// count trailing zeros
+		while (((temporaryCounter & 1) == 0) && (temporaryCounter != 0)) {
+			countZeros++;
+			temporaryCounter >>= 1;
+		}
+
+		// 00011 -> 0 -> 2 ones left to trailing zeros
+		// count ones left to trailing zeros
+		while ((temporaryCounter & 1) == 1) {
+			countOnes++;
+			temporaryCounter >>= 1;
+		}
+
+		// error checking
+		// if all the ones are in the left -> no bigger number can be created
+		// if the number is zero, no bit can be flipped
+		if (countZeros + countOnes == 31 || countZeros + countOnes == 0) {
+			return -1;
+		}
+
+		return number + (1 << countOnes) - (1 << (countZeros - 1)) + 1;
+
+	}
+
+	public static int getPrevArithmetic(int number) {
+		int temporaryCounter = number;
+		int countZeros = 0;
+		int countOnes = 0;
+
+		// count trailing ones
+		while ((temporaryCounter & 1) == 1) {
+			countOnes++;
+			temporaryCounter >>= 1;
+		}
+
+		// count zeros left of trailing ones
+		while (((temporaryCounter & 1) == 0) && (temporaryCounter != 0)) {
+			countZeros++;
+			temporaryCounter >>= 1;
+		}
+
+		return number - (1 << countOnes) - (1 << (countZeros - 1)) + 1;
+	}
+
+	public static int getNext(int number) {
+		int temporaryCounter = number;
+		int countZeros = 0;
+		int countOnes = 0;
+
+		// 00011000 -> 00011 -> 3 trailing zeros
+		// count trailing zeros
+		while (((temporaryCounter & 1) == 0) && (temporaryCounter != 0)) {
+			countZeros++;
+			temporaryCounter >>= 1;
+		}
+
+		// 00011 -> 0 -> 2 ones left to trailing zeros
+		// count ones left to trailing zeros
+		while ((temporaryCounter & 1) == 1) {
+			countOnes++;
+			temporaryCounter >>= 1;
+		}
+
+		// error checking
+		// if all the ones are in the left -> no bigger number can be created
+		// if the number is zero, no bit can be flipped
+		if (countZeros + countOnes == 31 || countZeros + countOnes == 0) {
+			return -1;
+		}
+
+		int flipPosition = countZeros + countOnes;
+
+		// flip the 0->1
+		number |= (1 << flipPosition);
+		// clear all the bits right of flipped bit
+		number &= ~((1 << flipPosition) - 1);
+		// set ones right to the position, but one less
+		number |= (1 << (countOnes - 1)) - 1;
+
+		return number;
+	}
+
+	public static int getPrev(int number) {
+		int temporaryCounter = number;
+		int countZeros = 0;
+		int countOnes = 0;
+
+		// count trailing ones
+		while ((temporaryCounter & 1) == 1) {
+			countOnes++;
+			temporaryCounter >>= 1;
+		}
+
+		// count zeros left of trailing ones
+		while (((temporaryCounter & 1) == 0) && (temporaryCounter != 0)) {
+			countZeros++;
+			temporaryCounter >>= 1;
+		}
+
+		int flipPosition = countZeros + countOnes;
+
+		// flip the 0->1
+		number &= ((~0) << (flipPosition + 1));
+
+		int mask = (1 << (countOnes + countZeros)) - 1;
+
+		// set ones right to the position, but one less
+		number |= mask << (countZeros - 1);
+
+		return number;
+	}
+
+}
---
+++ src/test/java/Chapter_05_Bit_Manipulation/NextNumberTest.java
@@ -0,0 +1,38 @@
+package Chapter_05_Bit_Manipulation;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NextNumberTest {
+
+	public static void print(String label, int input, int output) {
+		System.out.println(label + " " + Integer.toBinaryString(input) + "(" + input + ")\t\t->"
+				+ Integer.toBinaryString(output) + "(" + output + ")");
+	}
+
+	@Test
+	void testGetNextArithmetic() {
+		int input = 0b0111000111000;
+		print("next:\t\t\t", input, NextNumber.getNext(input));
+	}
+
+	@Test
+	void testGetPrevArithmetic() {
+		int input = 0b111000111000111;
+		print("prev:\t\t\t", input, NextNumber.getPrev(input));
+	}
+
+	@Test
+	void testGetNext() {
+		int input = 0b111000111000;
+		print("next arithmetic:\t", input, NextNumber.getNextArithmetic(input));
+	}
+
+	@Test
+	void testGetPrev() {
+		int input = 0b111000111000111;
+		print("prev arithmetic:\t", input, NextNumber.getPrevArithmetic(input));
+	}
+
+}
